8deade834a604f23b28a08c46837c0f883e4919b
Jasu <jasu.viding@helsinki.fi>
2021-11-13 04:02:56 +0200

    update api test features

--- app/front/app/api/api.tsx
+++ app/front/app/api/api.tsx
@@ -1,7 +1,32 @@
-export default class API {
+const API = {
 
-    getFileNames() {
-        return ['file_one.asm', 'file_two.asm', 'file_three.asm']
+    createFile: (filename: string) => {
+        return Promise.resolve(filename + '.asm')
+    },
+
+    deleteFile: () => { // filename: string
+        return Promise.resolve(true)
+    },
+
+    getFilenames: () => {
+        return Promise.resolve(['file_one.asm', 'file_two.asm', 'file_three.asm'])
+    },
+
+    getFileContents: () => { // filename: string
+        return Promise.resolve('asd\nasd\nasd')
+    },
+
+    saveFileContents: () => { // filename: string, contents: string
+        return Promise.resolve(true)
+    },
+
+    buildFile: () => { // filename: string
+        return Promise.resolve(true)
+    },
+
+    flashFile: () => { // filename: string
+        return Promise.resolve(true)
     }
-
 }
+
+export default API
