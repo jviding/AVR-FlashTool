7c0de064666c48416b503fae5aa0ef8ad121fbff
Jasu <jasu.viding@helsinki.fi>
2021-11-15 18:06:33 +0200

    api

--- app/api/src/api/endpoints/file.clj
+++ app/api/src/api/endpoints/file.clj
@@ -3,49 +3,56 @@
      [clojure.java.io :as io]
      [ring.util.response :refer [file-response, response, bad-request]]))
 
+(def uploadsDir "/uploads/")
+
 ;; PRIVATE
 
 (defn- fileExists [filename]
-    (.exists (io/file (str "/uploads/" filename))))
+    (.exists (io/file (str uploadsDir filename))))
 
-(defn- updateFileData [filename, data]
-    (with-open [w (io/writer (str "/uploads/" filename))]
+(defn- writeFile [filename, data]
+    (with-open [w (io/writer (str uploadsDir filename))]
         (.write w data)))
+
+(defn- readFile [filename]
+    (slurp (io/file (str uploadsDir filename))))
 
 ;; PUBLIC
 
 (defn getFilenames []
-    (let [filenames (.list (io/file "/uploads/"))]
+    (let [filenames (.list (io/file uploadsDir))]
         (response {:filenames (seq filenames)})))
 
 (defn getFile [filename]
-    (response {:filename filename}))
+    (cond 
+        (not (fileExists filename)) (bad-request "No such file")
+        :else (response {:filename filename, :data (readFile filename)})))
 
 (defn createFile [filename]
     (cond 
-        (not (.createNewFile (io/file (str "/uploads/" filename)))) (bad-request "Filename taken")
+        (not (.createNewFile (io/file (str uploadsDir filename)))) (bad-request "Filename taken")
         :else (response {:filename filename})))  
 
 (defn deleteFile [filename]
     (cond 
         (not (fileExists filename)) (bad-request "No such file")
-        (not (.delete (io/file (str "/uploads/" filename)))) (bad-request "Couldn't delete")
+        (not (.delete (io/file (str uploadsDir filename)))) (bad-request "Couldn't delete")
         :else (response {:message (str "Deleted " filename)})))
 
 (defn saveFile [filename, data]
     (cond
         (not (fileExists filename)) (bad-request "No such file")
-        :else (do (updateFileData filename data)
+        :else (do (writeFile filename data)
             (response {:message (str "Updated " filename)}))))
 
 (defn buildFile [filename, data]
     (cond
         (not (fileExists filename)) (bad-request "No such file")
-        :else (do (updateFileData filename data)
+        :else (do (writeFile filename data)
             (response {:message (str "Built " filename)}))))
 
 (defn flashFile [filename, data]
     (cond
         (not (fileExists filename)) (bad-request "No such file")
-        :else (do (updateFileData filename data)
+        :else (do (writeFile filename data)
             (response {:message (str "Flashed " filename)}))))
