c54a1b4149d786966d9cbf289e2fa559f05bb33d
Jasu <jasu.viding@helsinki.fi>
2021-11-18 21:56:55 +0200

    mcuLibs file and script

--- app/api/mcuReader.sh
+++ app/api/mcuReader.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+
 AVR_FILES_PATH="/usr/local/include/avr/"
 OUTPUT_FILE="mcuLibs.txt"
 
-# Create output file or clear if exists
-echo "TargetMCU Lib" > $OUTPUT_FILE
+## SCRIPT
 
-for FILE in $(ls "${AVR_FILES_PATH}")
+# Create output file or clear if exists
+echo -n "[" > $OUTPUT_FILE
+# Read AVR files
+FILES=$(ls "$AVR_FILES_PATH" -1 | grep '.inc')
+
+for FILE in $FILES
 do
     FILE_PATH="${AVR_FILES_PATH}${FILE}"
 
     # Read line matching 'Target MCU' from file
     TARGET_MCU=$(grep 'Target MCU' "$FILE_PATH")
     # Read substring from first occurrence of ':' until end
-    TARGET_MCU=${TARGET_MCU##*:}
+    TARGET_MCU="${TARGET_MCU##*:}"
     # Remove white spaces
-    TARGET_MCU=$(echo ${TARGET_MCU})
+    TARGET_MCU=$(echo "$TARGET_MCU" | xargs echo -n)
 
-    # Write "<Target MCU> <Lib>" to output file
-    echo "$TARGET_MCU $FILE" >> $OUTPUT_FILE
+    JSON="{\"mcu\":\"$TARGET_MCU\",\"lib\":\"$FILE\"},"
+    echo "$JSON" >> $OUTPUT_FILE
 done
+
+# Remove last ','
+$(truncate -s-2 $OUTPUT_FILE)
+echo -n "]" >> $OUTPUT_FILE
